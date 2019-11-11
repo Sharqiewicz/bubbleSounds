a98f6a4fb06b4001c6be65c5bc6c9b37d87615c0
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2019-11-11 14:44:17 +0100

    Animation done

---
+++ script.js
@@ -0,0 +1,24 @@
+const bubbles = document.querySelectorAll('.bubbles div');
+const colors = ['#845EC2','#D65DB1','#FF6F91', '#FF9671', '#FFC75F', '#F9F871'];
+
+const fatherEl = document.querySelector('.container');
+
+
+bubbles.forEach( (bubble, index) => {
+    bubble.addEventListener('click', function sendBubble(){
+
+            let sentbb = document.createElement('div');
+            sentbb.classList.add('sentbb');
+            sentbb.style.background = colors[index];
+            sentbb.style.animation = 'sent 1s ease-in-out';
+
+            fatherEl.appendChild(sentbb);
+
+
+            document.querySelector('.animation .bigBubble').style.background = colors[index];
+
+            sentbb.addEventListener('animationend', function(){
+                fatherEl.removeChild(sentbb);
+            })
+    })
+} )
